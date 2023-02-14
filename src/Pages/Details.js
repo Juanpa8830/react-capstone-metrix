cf32aa3952e241301ddbd74a10d995cad63a8891
Juanpa8830 <jp3_4@hotmail.com>
2023-02-14 15:38:46 -0500

    Add  Details component and render API info

--- src/Pages/Details.js
+++ src/Pages/Details.js
@@ -9,12 +9,52 @@ const Details = () => {
 
   return (
 
-    <div className="details">
-      <Link to="/" className="arrow-back">
-        <button type="button"><img src={leftarrow} alt="back-home" /></button>
-      </Link>
-      <h1 className="details-title">{`${country.name}`}</h1>
+    <div className="detailscontainer">
+      <div className="Subregion">Subregion</div>
+      <p className="dato">{country.subregion}</p>
+      <div className="Coordenates">Coordenates</div>
+      <p className="dato2">
+        {country.coordenates.lat}
+        ,
+        {country.coordenates.long}
+      </p>
+      <div className="Population">Population</div>
+      <p className="dato3">
+        {country.population}
+        {' '}
+        habs
+      </p>
+
+      <div className="Car-side">Car-side</div>
+      <p className="dato4">{country.carside}</p>
+
+      <div className="Start-of-week">Start of week</div>
+      <p className="dato-5">{country.soweek}</p>
+      <div className="capital">Capital</div>
+      <p className="dato-6">{country.capital}</p>
+      <div className="Coat-of-Arms">Coat of Arms</div>
+      <div className="img-escudo" id="flex">
+        <img
+          src={country.coarms}
+          width="200px"
+          height="200px"
+          alt="coatofarms"
+        />
+        <Link to="/" className="arrow-back">
+          <button type="button">
+            <img
+              className="arrowback"
+              src={leftarrow}
+              alt="back-home"
+              width="50px"
+              height="50px"
+            />
+          </button>
+        </Link>
+
+      </div>
     </div>
+
   );
 };
 
